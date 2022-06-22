7d79aa0a6a5ee6df6d0c4c98987ee7ce7d2fc1e3
FiliP <filip.ozbolt@windowslive.com>
2022-06-23 01:56:24 +0200

    code push from local repo

---
+++ src/db.js
@@ -0,0 +1,31 @@
+import mongo from 'mongodb';
+
+let connection_string = 'mongodb+srv://admin:admin@cluster1.jniy7.mongodb.net/?retryWrites=true&w=majority';
+//let connection_string = 'localhost:3000/fipugram'; 
+
+let client = new mongo.MongoClient(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let db = null;
+
+// eksportamo Promise koji resolva na konekciju
+export default () => {
+    return new Promise((resolve, reject) => {
+        // ako smo inicijalizirali bazu i klijent je još uvijek spojen
+        if (db && client.isConnected()) {
+            resolve(db);
+        } else {
+            client.connect((err) => {
+                if (err) {
+                    reject('Spajanje na bazu nije uspjelo:' + err);
+                } else {
+                    console.log('Database connected successfully!');
+                    db = client.db('fipugram');
+                    resolve(db);
+                }
+            });
+        }
+    });
+};
