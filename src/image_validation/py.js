f88680339e1e6bfe5dd1738bfe562106b22bdb41
FiliP <filip.ozbolt@windowslive.com>
2022-06-29 20:14:37 +0200

    py-js update

--- src/image_validation/py.js
+++ src/image_validation/py.js
@@ -1,8 +1,11 @@
 const { spawn } = require('child_process');
 
+//treba imati cwd jer inace ne executa python skriptu osim ako ju ne zovemo bas iz foldera u kojem se nalazi
+process.chdir('\image_validation');
 const command = spawn('python', ["./test.py", 1, 2]);
 
 let result = '';
+
 
 command.stdout.on('data', function (data) {
   result += data.toString();
